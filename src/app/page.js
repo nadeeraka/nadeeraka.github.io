01f7f9d3905f57d81d741ef284d2dc04b9f3d7a8
nadeeraka <nadeerakan@gmail.com>
2023-05-21 19:05:00 +0530

    ui change

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 "use client"; // This is a client component 👈🏽
-import {useState} from 'react'
+import { useState } from "react";
 import Image from "next/image";
 import {
   BsFillMoonStarsFill,
+  BsFillSunFill,
   BsGithub,
   BsLinkedin,
   BsTwitter,
@@ -18,33 +19,45 @@ import web4 from "../../public/web4.png";
 import web5 from "../../public/web5.png";
 import web6 from "../../public/web6.png";
 
-
-
-
-
-
 export default function Home() {
-
-  const [darkMode, setdarkMode] = useState(true)
+  const [darkMode, setdarkMode] = useState(true);
   return (
     <div
-    //  className={darkMode? 'dark':''}
-    className='bg-black'
-    
+      //  className={darkMode? 'dark':''}
+      className="bg-black"
     >
-      <head  className='bg-black'>
+      <head className="bg-black">
         <title className="font-burtons">Nimantha Nadeeraka Portofolio</title>
       </head>
 
-      <main  className={darkMode? 'bg-black py-20 p-2 md:p-5 lg:p-10':'bg-white py-20 p-2 md:p-5 lg:p-10'} >
+      <main
+        className={
+          darkMode
+            ? "bg-black py-20 p-2 md:p-5 lg:p-10"
+            : "bg-white py-20 p-2 md:p-5 lg:p-10"
+        }
+      >
         <div className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1  className={darkMode? 'text-white  text-xl font-burtons md:text-2xl lg:text-4xl':' text-xl font-burtons md:text-2xl lg:text-4xl'} >
+            <h1
+              className={
+                darkMode
+                  ? "text-white  text-xl font-burtons md:text-2xl lg:text-4xl"
+                  : " text-xl font-burtons md:text-2xl lg:text-4xl"
+              }
+            >
               Nimantha Nadeeraka
             </h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={()=>setdarkMode(!darkMode)} className="cursor-pointer md:text-2xl lg:text-2xl" />
+                <BsFillSunFill
+                  onClick={() => setdarkMode(!darkMode)}
+                  className={
+                    darkMode
+                      ? "text-white cursor-pointer md:text-2xl lg:text-4xl"
+                      : " cursor-pointer md:text-2xl lg:text-4xl"
+                  }
+                />
               </li>
               <li>
                 {" "}
@@ -60,11 +73,13 @@ export default function Home() {
           <div className="text-center p-10">
             <h2 className="text-5xl text-teal-500 py-4">Nimantha Nadeeraka</h2>
             <h3 className="text-4xl  py-4 ">Software Engineer</h3>
-            <p className="text-2xl text-gray-500 text-md mb-6 ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              voluptatum sed veritatis eos atque laudantium quaerat asperiores
-              officiis, necessitatibus minima suscipit repudiandae deleniti
-              ipsam non exercitationem delectus nam voluptatem ratione.
+            <p className="mb-10">
+              <span className="text-4xl text-gray-500 text-md mb-6  mx-auto max-w-20 ">
+                “Simplicity is the ultimate sophistication”{" "}
+              </span>
+              <span className="text-1xl text-gray-500 ">
+                (Leonardo Da Vinci)
+              </span>
             </p>
 
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
@@ -87,17 +102,15 @@ export default function Home() {
 
           <div className="text-center text-2x py-10 text-gray-500 text-md lg:text-2xl mx-w-xl mx-auto mt-10  ">
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Praesentium dicta incidunt quo debitis id! Iure nemo tempore
-              cumque! Asperiores esse reiciendis repellat ad perspiciatis
-              mollitia pariatur minus voluptatem soluta eligendi.
-            </p>
-
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-              accusantium quos, et ex maiores aperiam ab temporibus porro
-              quisquam tempore cupiditate magni totam sint provident possimus,
-              rem cum quae nemo.
+              The phrase "simplicity is the ultimate sophistication" implies
+              that achieving simplicity in design, thought, or expression is a
+              mark of true sophistication and mastery. It means distilling
+              complex ideas or systems into their most fundamental and intuitive
+              form, removing unnecessary elements while retaining functionality
+              and elegance. It suggests that sophistication lies in the ability
+              to simplify and communicate effectively, emphasizing the value of
+              simplicity as an elegant, clear, and powerful goal to strive for
+              in different aspects of life.
             </p>
           </div>
           {/* 
@@ -116,10 +129,12 @@ export default function Home() {
                 Clean Code with best practice
               </h3>
               <p className=" p-5 text-center text-gray-500">
-                Throughout my venture as a software engineer, I have seen people
-                code in various ways. Sometimes it’s easier to read and
-                understand while others took a considerable time to go through.
-             
+                Clean Code refers to the practice of writing code that is easy
+                to understand, maintain, and modify. It emphasizes the
+                importance of readability, simplicity, and clarity in code
+                structure and organization. Clean Code follows a set of best
+                practices that promote efficient and effective software
+                development.
               </p>
             </div>
             <div className=" bg-gray-100 shadow-lg shadow-indigo-500/40 ... rounded-xl p-10 my-10  mx-10 md:mx-20 lg:mx-40">
@@ -128,10 +143,14 @@ export default function Home() {
                 Elegant designs{" "}
               </h3>
               <p className=" p-5 text-center text-gray-500">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-                minus a sed adipisci cupiditate molestiae, in corporis facere
-                dicta, impedit, excepturi dolores illo voluptatum quaerat
-                
+                Elegant website designs exhibit visually pleasing and
+                sophisticated layouts that provide a seamless user experience.
+                They are characterized by visual harmony, minimalism, intuitive
+                navigation, thoughtfully selected typography, high-quality
+                imagery, and responsive design. Consistency, attention to
+                detail, and a focus on user experience are key elements. Elegant
+                designs create a cohesive and polished aesthetic while
+                effectively conveying the website's purpose and content.
               </p>
             </div>
             <div className="bg-gray-100 shadow-lg shadow-indigo-500/40 ... rounded-xl p-10 my-10  mx-10 md:mx-20 lg:mx-40">
@@ -145,9 +164,12 @@ export default function Home() {
                 Consulting{" "}
               </h3>
               <p className=" p-5 text-center text-gray-500">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-                minus a sed adipisci cupiditate molestiae, in corporis facere
-                
+                Good consulting refers to the practice of providing expert
+                advice and guidance to individuals, organizations, or businesses
+                to help them solve problems, make informed decisions, and
+                achieve their goals. A good consulting service involves a deep
+                understanding of the client's needs and objectives, combined
+                with industry knowledge and expertise.
               </p>
             </div>
           </div>
@@ -177,27 +199,63 @@ portfolio
           </p>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap gap-40">
             <div className="basis-1/3 flex-1">
-              <Image src={web1} className='rounded-lg object-cover' width={'100%'} hight={'100%'} layout='responsive' />
+              <Image
+                src={web1}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                hight={"100%"}
+                layout="responsive"
+              />
             </div>
 
             <div className="basis-1/3 flex-1">
-              <Image src={web2} className='rounded-lg object-cover' width={'100%'} hight={'100%'} layout='responsive'  />
+              <Image
+                src={web2}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                hight={"100%"}
+                layout="responsive"
+              />
             </div>
 
             <div className="basis-1/3 flex-1">
-              <Image src={web3} className='rounded-lg object-cover' width={'100%'} hight={'100%'} layout='responsive' />
+              <Image
+                src={web3}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                hight={"100%"}
+                layout="responsive"
+              />
             </div>
 
             <div className="basis-1/3 flex-1">
-              <Image src={web4} className='rounded-lg object-cover' width={'100%'} hight={'100%'} layout='responsive'  />
+              <Image
+                src={web4}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                hight={"100%"}
+                layout="responsive"
+              />
             </div>
 
             <div className="basis-1/3 flex-1">
-              <Image src={web5} className='rounded-lg object-cover' width={'100%'} hight={'100%'} layout='responsive' />
+              <Image
+                src={web5}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                hight={"100%"}
+                layout="responsive"
+              />
             </div>
 
             <div className="basis-1/3 flex-1">
-              <Image src={web6} className='rounded-lg object-cover' width={'100%'} hight={'100%'} layout='responsive' />
+              <Image
+                src={web6}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                hight={"100%"}
+                layout="responsive"
+              />
             </div>
           </div>
         </div>
